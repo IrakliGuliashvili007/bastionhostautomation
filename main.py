ef6145aab1817d35b8e70f2c7305173d67565a11
IrakliGuliashvili007 <128288227+IrakliGuliashvili007@users.noreply.github.com>
2023-06-20 06:33:32 +0000

    a

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import boto3
-from botocore.exceptions import ClientError
-import time
-
-
 def create_db_subnet_group(rds_client, subnet_group_name, vpc_id, subnet_ids):
   description = "auto-description"
   response = rds_client.create_db_subnet_group(
@@ -23,7 +18,7 @@ def create_rds_security_group(ec2_client, security_group_name, vpc_id,
     print(
       f"Security Group {security_group_name} already exists with ID {security_group_id}"
     )
-  except ClientError as e:
+  except ec2_client.exceptions.ClientError as e:
     if e.response['Error']['Code'] == 'InvalidGroup.NotFound':
       response = ec2_client.create_security_group(
         GroupName=security_group_name,
@@ -36,24 +31,19 @@ def create_rds_security_group(ec2_client, security_group_name, vpc_id,
     else:
       raise e
 
-  try:
-    ec2_client.authorize_security_group_ingress(GroupId=security_group_id,
-                                                IpPermissions=[{
-                                                  'IpProtocol':
-                                                  'tcp',
-                                                  'FromPort':
-                                                  5432,
-                                                  'ToPort':
-                                                  5432,
-                                                  'UserIdGroupPairs': [{
-                                                    'GroupId':
-                                                    ec2_security_group_id
-                                                  }]
-                                                }])
-  except ClientError as e:
-    print(
-      f"Error authorizing ingress for Security Group {security_group_name}: {str(e)}"
-    )
+  ec2_client.authorize_security_group_ingress(GroupId=security_group_id,
+                                              IpPermissions=[{
+                                                'IpProtocol':
+                                                'tcp',
+                                                'FromPort':
+                                                5432,
+                                                'ToPort':
+                                                5432,
+                                                'UserIdGroupPairs': [{
+                                                  'GroupId':
+                                                  ec2_security_group_id
+                                                }]
+                                              }])
 
   return security_group_id
 
@@ -85,38 +75,33 @@ def create_db_instance(rds_client, security_group_id, subnet_group_name):
     DeletionProtection=False)
 
   db_instance_id = response['DBInstance']['DBInstanceIdentifier']
-  print(f"Waiting for DB instance {db_instance_id} to be available...")
-
-  while True:
-    try:
-      response = rds_client.describe_db_instances(
-        DBInstanceIdentifier=db_instance_id)
-      status = response['DBInstances'][0]['DBInstanceStatus']
-      if status == 'available':
-        print(f"RDS instance {db_instance_id} is available now.")
-        break
-      else:
-        print(
-          f"Current status of RDS instance {db_instance_id}: {status}. Waiting..."
-        )
-        time.sleep(10)
-    except ClientError as e:
-      print(f"Error occurred while checking RDS instance status: {str(e)}")
-      break
+  print("Waiting for DB instance to be available...")
+  waiter = rds_client.get_waiter('db_instance_available')
+  waiter.wait(DBInstanceIdentifier=db_instance_id)
+  print(f"RDS instance {db_instance_id} was created")
 
 
 # Usage example
+import boto3
+
+# Create RDS and EC2 clients
 rds_client = boto3.client('rds')
 ec2_client = boto3.client('ec2')
 
+# Set the required parameters
 subnet_group_name = 'my-db-subnet-group'
 vpc_id = 'vpc-12345678'
 subnet_ids = ['subnet-abcdef', 'subnet-ghijklm']
 security_group_name = 'my-db-security-group'
 ec2_security_group_id = 'sg-12345678'
 
+# Create the DB subnet group
 subnet_group_name = create_db_subnet_group(rds_client, subnet_group_name,
                                            vpc_id, subnet_ids)
+
+# Create the RDS security group
 security_group_id = create_rds_security_group(ec2_client, security_group_name,
                                               vpc_id, ec2_security_group_id)
+
+# Create the RDS instance
 create_db_instance(rds_client, security_group_id, subnet_group_name)
